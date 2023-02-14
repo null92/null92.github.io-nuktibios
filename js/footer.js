5535f142f96dc9f6f4c713d0524bf9c14a9055fd
null92 <49656826+null92@users.noreply.github.com>
2023-02-14 18:53:56 -0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ class Footer extends HTMLElement {
             <a target="_blank" rel="noopener noreferrer" style="bottom: 15px; right: 15px; position: fixed; z-index:1" href="https://wa.me/5511945413759"><img style="height: 50px; width: 50px" src="img/whatsapp-logo.png" alt="WhatsApp logo"></a>
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-12 text-lg-center">&copy; 2022 Nuktibios Art</div>
+                    <div class="col-lg-12 text-lg-center">&copy; 2023 Nuktibios Art</div>
                     <p></p>
                 </div>
             </div>
