fd21430be57865c402251826ca032dd2f7f2840d
null92 <49656826+null92@users.noreply.github.com>
2025-10-03 12:21:58 -0300

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,11 @@
 class Footer extends HTMLElement {
   connectedCallback() {
+    const currentYear = new Date().getFullYear();
     this.innerHTML = `<footer class="footer py-4">
             <a target="_blank" rel="noopener noreferrer" style="bottom: 15px; right: 15px; position: fixed; z-index:1" href="https://wa.me/5511945413759"><img style="height: 50px; width: 50px" src="img/whatsapp-logo.png" alt="WhatsApp logo"></a>
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-12 text-lg-center">&copy; Nuktibios Art</div>
+                    <div class="col-lg-12 text-lg-center">Nuktibios Art &copy; ${currentYear}</div>
                     <p></p>
                 </div>
             </div>
