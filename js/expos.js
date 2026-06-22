a17c8c0dce46d0dc4a5a6d1a632cf57e29f184ae
null92 <49656826+null92@users.noreply.github.com>
2026-06-22 02:43:47 -0300

    Refactor expos section layout for center alignment

--- js/expos.js
+++ js/expos.js
@@ -6,20 +6,20 @@ class Expos extends HTMLElement {
                     <h2 class="section-heading text-uppercase">Exposições</h2>
                     <h3 class="section-subheading text-muted"></h3>
                 </div>
-                <div class="row">
-                    <div class="col-lg-4 col-sm-6 mb-4"></div>
-                    <div class="col-lg-4 col-sm-6 mb-4">
-                        <div class="art-item">
-                            <a class="art-link" data-toggle="modal" href="#expoModal1">
-                                <img class="img-fluid" src="img/expos/1/01-thumbnail.png" alt="" />
-                            </a>
-                        </div>
-                        <div class="art-caption">
-                            <div class="art-caption-heading text-center">Expo Arte SP 20ª Edição</div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-sm-6 mb-4"></div>
-                </div>
+                <div class="row justify-content-center">
+                  <div class="col-lg-4 col-sm-6 mb-4">
+                      <div class="art-item">
+                          <a class="art-link" data-toggle="modal" href="#expoModal1">
+                              <img class="img-fluid" src="img/expos/1/01-thumbnail.png" alt="">
+                          </a>
+                      </div>
+                      <div class="art-caption">
+                          <div class="art-caption-heading text-center">
+                              Expo Arte SP 20ª Edição
+                          </div>
+                      </div>
+                  </div>
+              </div>
             </div>
         </section>`;
   }
